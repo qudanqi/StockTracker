624fe2430ffab83dcfaa3a5b6b45a95fc95e3da4
liecn <lichenni@msu.edu>
2020-11-16 11:28:58 -0500

    init

---
+++ StockTracker/StockTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockTracker
+//
+//  Created by Chenning Li on 11/16/20.
+//  Copyright © 2020 Chenning Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
